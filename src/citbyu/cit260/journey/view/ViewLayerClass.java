957a95d2f5d316078eb261aa3351617dcb3d432a
Jared <Jared@Cybertron>
2016-06-18 22:15:07 -0600

    New View layer class

---
+++ src/citbyu/cit260/journey/view/ViewLayerClass.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package citbyu.cit260.journey.view;
+
+import java.util.Scanner; 
+
+/**
+ *
+ * @author Jared
+ */
+public class ViewLayerClass implements ViewInterface {
+
+    @Override
+    public void display() {
+        //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public String getInput() {
+        //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public boolean doAction(String value) {
+        //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet."); 
+       
+    }
+    
+ 
+    
+    
+}
