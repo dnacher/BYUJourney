bedfe43d8c3a24a620b29dc13d5c21d75f351108
Dani-Fla-Mathi <Dani-Fla-Mathi@PDani-Fla-Mathi>
2016-06-22 12:47:57 -0300

    clean some classes and remove others that weren´t useful right now

--- src/citbyu/cit260/journey/control/controlPlace.java
+++ src/citbyu/cit260/journey/control/controlPlace.java
@@ -1,10 +1,5 @@
 package citbyu.cit260.journey.control;
 
-import citbyu.cit260.journey.model.characters.Warrior;
-import citbyu.cit260.journey.model.characters.enums.Warriors;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  *
  * @author Dani-Fla-Mathi
@@ -21,21 +16,6 @@ public class controlPlace {
     int area=height*width*depth;
     percentage-=area/powerLight;    
     return percentage;
-    }
-    
-    public List<Warrior> moveEnemies(List<Warrior> originalList){
-        List<Warrior> newList= new ArrayList();
-        Warrior w= new Warrior(Warriors.FRIENDLY);
-        for(int i=0; i<originalList.size();i++){
-            newList.add(i, w);
-            newList.add(i+1, w);
-            newList.add(i+2, w);
-            newList.add(i+3, originalList.get(i));
-        }
-        return newList;
-    }
-    
-    public controlPlace(){
     }
     
 }
